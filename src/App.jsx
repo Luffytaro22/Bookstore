2a41672ba9a04f7384594fcaa111f97c600c3d4e
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-13 11:08:37 -0500

    Added the Home route and the NavBar component to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css'
+import './App.css';
+import NavBar from './components/navBar';
+import Home from './Routes/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
         <Route path="categories" />
+      </Route>
     </Routes>
   )
 }
