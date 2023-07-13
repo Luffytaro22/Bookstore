832fb704a0d8d5ea9662d6fb25c361272d9b3f97
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-13 18:40:15 -0500

    Move Home component to components directory

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import RenderBooks from './renderBooks';
+import CreateBooks from './createBooks';
+
+export default function Home() {
+  return (
+    <>
+      <RenderBooks />
+      <CreateBooks />
+    </>
+  );
+}
