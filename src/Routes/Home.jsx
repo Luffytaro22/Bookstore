34305f047aafa534c801c0d4f9e67fa237f4645d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-13 11:08:01 -0500

    Created a Home route

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,11 @@
+import RenderBooks from '../components/renderBooks';
+import CreateBooks from '../components/createBooks';
+
+export default function Home() {
+  return (
+    <>
+      <RenderBooks />
+      <CreateBooks />
+    </>
+  );
+}
