fd0372e41bdaab916ca8ca5bcab88efa2e45518f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-17 14:46:45 -0500

    Created the booksSlice state

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Slice state for books
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      return state.filter((book) => book.id !== action.payload);
+    },
+  },
+});
+
+// Export the reducers of the books slice
+export const { addBook, removeBook } = booksSlice.actions;
