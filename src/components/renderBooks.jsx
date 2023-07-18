1d2d817d899d4c8de4cb4efc90a34148f0ebbd42
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-18 09:47:13 -0500

    Select the books reducer and display each books of the initial state

--- src/components/renderBooks.jsx
+++ src/components/renderBooks.jsx
@@ -1,29 +1,14 @@
+import { useSelector } from 'react-redux';
 import Books from './Books';
 import styles from '../styles/renderBooks.module.css';
 
 export default function RenderBooks() {
-  const books = [
-    {
-      gender: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      gender: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      gender: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-    },
-  ];
+  const books = useSelector((state) => state.books);
 
   return (
     <div className={styles.books}>
       {books.map((book) => (
-        <Books key={book.title} gender={book.gender} title={book.title} author={book.author}/>
+        <Books key={book.item_id} gender={book.category} title={book.title} author={book.author}/>
       ))}
     </div>
   );
