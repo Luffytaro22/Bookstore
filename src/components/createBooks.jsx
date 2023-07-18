f87edf6583ad9692f5104f2b66512afc3cfad4ca
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-18 10:26:32 -0500

    Created a handleClick function to add a new book when the user press the ADD BOOk button

--- src/components/createBooks.jsx
+++ src/components/createBooks.jsx
@@ -1,14 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 import styles from '../styles/createBooks.module.css';
 
 export default function CreateBooks() {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
+  const handleClick = (event) => {
+    event.preventDefault();
+    const title = document.getElementById('bookTitle');
+    const author = document.getElementById('bookAuthor');
+    if (title.value && author.value) {
+      dispatch(addBook({
+        "item_id": `item${books.length + 1}`,
+        "title": title.value,
+        "author": author.value,
+        "category": "Drama",
+      }));
+      title.value = '';
+      author.value = '';
+    }
+  };
+
   return (
     <div className={styles.formContainer}>
       <hr />
       <h3>ADD NEW BOOK</h3>
       <form>
-        <input type="text" placeholder="Book title" className={styles.formFields} />
-        <input type="text" placeholder="Author" className={styles.formFields} />
-        <button type="submit" className={styles.submitButton}>ADD BOOK</button>
+        <input type="text" placeholder="Book title" className={styles.formFields} id="bookTitle" required />
+        <input type="text" placeholder="Author" className={styles.formFields} id="bookAuthor" required />
+        <button type="submit" className={styles.submitButton}onClick={handleClick} >ADD BOOK</button>
       </form>
     </div>
   );
